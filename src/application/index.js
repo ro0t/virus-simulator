16da0158d06deeb580394053bca8f2f3d7a03a6b
Adam <adam@igital.co>
2018-09-16 21:02:50 +0000

    Removed components and added game loop

--- src/application/index.js
+++ src/application/index.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
-// Navigation component example
-import Navigation from '../components/Navigation';
+const GAME_WIDTH = 600;
+const GAME_HEIGHT = 600;
 
-// Import pages
-import IndexPage from '../pages/IndexPage';
-import AboutPage from '../pages/AboutPage';
+const GameScreen = styled.div`
+	width: ${GAME_WIDTH}px;
+	height: ${GAME_HEIGHT}px;
+	background: #fbfbfb;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	box-shadow: 0 0 100px rgba(0,0,0,.1);
+`;
+
+const Atom = styled.div`
+	font: consolas;
+	display: block;
+	position: absolute;
+	width: ${GAME_WIDTH * 0.02}px;
+	height: ${GAME_HEIGHT * 0.02}px;
+	color: green;
+	left: 10px;
+	top: 10px;
+	font-size: ${GAME_HEIGHT * 0.04}px;
+	z-index: 2;
+`;
+
+let atoms = [];
 
 class AppContainer extends React.Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			atom: 0
+		}
+	}
+
+	componentDidMount() {
+		this.setState(state => {
+			state.atom++;
+		});
+
+		//atoms[] = new Atom();
+	}
+
 	render() {
+
+		const { cycle } = this.props;
+
 		return (
 			<div>
-
-				<Navigation />
-
-				<Switch>
-					<Route exact path="/" component={IndexPage} />
-					<Route exact path="/about" component={AboutPage} />
-				</Switch>
-
+				<GameScreen>
+					<Atom>@</Atom>
+				</GameScreen>
+				<span>Cycle: {cycle}</span>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './application';
 
-render((
-	<BrowserRouter>
-		<AppContainer />
-	</BrowserRouter>
-), document.getElementById('app'))
+let cycle = 0;
+
+function update(progress) {
+  // Update the state of the world for the elapsed time since last render
+}
+
+function draw() {
+  // Draw the state of the world
+  render((
+  	<AppContainer cycle={cycle} />
+  ), document.getElementById('app'))
+}
+
+function loop(timestamp) {
+  var progress = timestamp - cycle
+
+  update(progress)
+  draw()
+
+  cycle = timestamp
+  window.requestAnimationFrame(loop)
+}
+
+window.requestAnimationFrame(loop)
